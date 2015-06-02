0271803725cafce5f3ad5570bdf49039d96e9bcc
Pierre-Alexis de Solminihac <pa@quai13.com>
2015-06-02 10:56:15 +0200

    BOOTSTRAP3 1.2 fix notice when jquerydatatables is not installed

--- view/bootstrap3/cssjs.php
+++ view/bootstrap3/cssjs.php
@@ -19,10 +19,10 @@ if ($cssjs->is_registered_foot('jquery.dataTables')) {
     $cssjs->register_foot('bootstrap3datatables', array(
         'src' => __WWW_ROOT_JQUERYDATATABLES__ . '/skin/js/dataTables.bootstrap.js',
     ));
+    $cssjs->register_css('bootstrap3datatables', array(
+        'src' => __WWW_ROOT_JQUERYDATATABLES__ . '/skin/css/dataTables.bootstrap.css'
+    ));
 }
-$cssjs->register_css('bootstrap3datatables', array(
-    'src' => __WWW_ROOT_JQUERYDATATABLES__ . '/skin/css/dataTables.bootstrap.css'
-));
 // adaptations clementine
 $cssjs->register_css('bootstrap3skin', array(
     'src' => __WWW_ROOT_BOOTSTRAP3__ . '/skin/css/clementine-style.css'
